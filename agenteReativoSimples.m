a308885e3524a310a7152dbe0f523c467f9c87cb
Renato Constancio Filho <renatoconstanciofilho@gmail.com>
2018-09-13 10:36:00 -0300

    Alterado a matriz de ações de , para ;

--- agenteReativoSimples.m
+++ agenteReativoSimples.m
@@ -8,20 +8,20 @@ function acao = agenteReativoSimples(percepcao)
 % ações = {'acima', 'abaixo', 'esquerda', 'direita', 'aspirar'}
 %            1         2          3           4          5
 
-funcaoMapear = [2 2 4,
-                2 3 4,
-                2 4 4,
-                2 5 2,
-                3 5 3,
-                3 4 3,
-                3 3 3,
-                3 2 2,
-                4 2 4,
-                4 3 3,
-                4 4 3,
-                4 5 2,
-                5 5 3,
-                5 4 3,
+funcaoMapear = [2 2 4;
+                2 3 4;
+                2 4 4;
+                2 5 2;
+                3 5 3;
+                3 4 3;
+                3 3 3;
+                3 2 2;
+                4 2 4;
+                4 3 3;
+                4 4 3;
+                4 5 2;
+                5 5 3;
+                5 4 3;
                 5 3 3]
                 
 mapeamento = 0; %para verificar erro
